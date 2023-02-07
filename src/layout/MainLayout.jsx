3e2551306c15211c7fbc0a9e0534790668f938df
mauricio <jourdanmauricio@gmail.com>
2023-02-07 13:48:57 -0300

    Add title, description and favicon

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,20 @@
 import Hero from '@/components/Hero';
 import Nav from '@/common/Nav';
+import Head from 'next/head';
 // import Footer from '../common/footer/Footer';
 
 const MainLayout = ({ children }) => {
   return (
     <>
       <div className="min-h-full">
+        <Head>
+          <title>Hatha Yoga Lobería - Yoga y Shiatsu</title>
+          <meta
+            name="description"
+            content="Yoga en Lobería. Recuerda, el yoga es una luz, que una vez encendida, nunca se atenuará. Cuanto mejor es la práctica, más brillante es la llama"
+          />
+          <link rel="icon" type="image/png" href="/favicon.png"></link>
+        </Head>
         <Nav />
         <Hero />
         <main>
