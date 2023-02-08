d3e61fc0d0358c3cbb168eab9dba60a5483ff039
mauricio <jourdanmauricio@gmail.com>
2023-02-07 21:27:30 -0300

    Add Open Graph

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -9,24 +9,33 @@ const MainLayout = ({ children }) => {
       <div className="min-h-full">
         <Head>
           <title>Hatha Yoga Lobería - Yoga y Shiatsu</title>
+          {/* google */}
           <meta name="title" content="Hatha Yoga Lobería - Yoga y Shiatsu" />
           <meta
             name="description"
             content="Yoga en Lobería. Recuerda, el yoga es una luz, que una vez encendida, nunca se atenuará. Cuanto mejor es la práctica, más brillante es la llama"
           />
-
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://hathayogaloberia.com.ar/" />
+          {/* facebook */}
           <meta
             property="og:title"
             content="Hatha Yoga Lobería - Yoga y Shiatsu"
           />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://hathayogaloberia.com.ar/" />
           <meta
             property="og:description"
             content="Yoga en Lobería. Recuerda, el yoga es una luz, que una vez encendida, nunca se atenuará. Cuanto mejor es la práctica, más brillante es la llama"
           />
-          <meta property="og:image" content="" />
+          <meta
+            property="og:image"
+            content="https://hathayogaloberia.com.ar/galeria2.jpeg"
+          />
+          <meta property="og:image:type" content="image/jpeg" />
+          <meta property="og:image:width" content="563" />
+          <meta property="og:image:heght" content="350" />
+          <meta property="og:image:alt" content="Posturas de yoga" />
 
+          {/* twitter */}
           <meta property="twitter:card" content="summary_large_image" />
           <meta
             property="twitter:url"
